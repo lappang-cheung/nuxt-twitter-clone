b8a5eda4be3949248191d9776d8622be60fedb0a
leocheung <lappangcheung@gmail.com>
2022-12-13 01:52:17 -0500

    learning primsa with nuxt

---
+++ components/Sidebar/Left/Tab.vue
@@ -0,0 +1,27 @@
+<template>
+  <nuxt-link to="#" 
+             class="flex items-center p-3 w-min hover:bg-gray-200 rounded-full dark:hover:bg-dim-200 dark:text-white text-black" 
+             :class="defaultTransition">
+    <div class="h-6 w-6 text-dark">
+      <slot name="icon" />
+    </div>
+    <div class="ml-4 text-xl hidden xl:block" 
+         :class="textClasses">
+      <slot name="name" />
+    </div>
+  </nuxt-link>  
+</template>
+<script setup>
+const { defaultTransition } = useTailwindConfig()
+
+const props = defineProps({
+  active: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const textClasses = computed(() => {
+  props.active ? 'font-semibold' : 'font-normal'
+})
+</script>
---
+++ composables/useTailwindConfig.js
@@ -0,0 +1,6 @@
+export default () => {
+  return {
+    defaultTransition: 'transition ease-in-out duriation-350',
+    twitterBorderColor: 'border-white-200 dark:border-gray-700'
+  }
+}
